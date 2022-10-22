de8343c186b3382be088f0a17dc8431b1677c1eb
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-10-22 13:56:11 +0800

    Re-organized the codes that are reiterated and adds more context to asterisk display name design.

--- 2dasterisk.py
+++ 2dasterisk.py
@@ -4,42 +4,37 @@
 
 # REFERENCE IN 2D PLOT
 
-# print("*****  *****  ****")
-# print("*      *      *   *")
-# print("*      *****  *   *")
-# print("*      *      *   *")
-# print("*****  *****  ****")
+# print("*******  *******  ****")
+# print("*     *  *        *   *")
+# print("*        *        *    *")
+# print("*        *****    *     *")
+# print("*     *  *        *    *")
+# print("*     *  *        *   *")
+# print("*******  *******  ****")
 
 displayName = "CED"
-print_C = [[" " for int_col in range(5)] for int_row in  range(5)]
-print_E = [[" " for int_col in range(5)] for int_row in  range(5)]
-print_D = [[" " for int_col in range(5)] for int_row in  range(5)]
+print_C = [[" " for int_col in range(7)] for int_row in  range(7)]
+print_E = [[" " for int_col in range(7)] for int_row in  range(7)]
+print_D = [[" " for int_col in range(7)] for int_row in  range(7)]
 
-#code for C
-for row in range(5):
-    for col in range(5):
-        if (col == 0 or (row == 0 or row == 4) and (col > 0)):
-            print_C[row][col] = "*"
+for row in range(7):
+    for col in range(7):
+        if ((col == 0 and row != 0) or (row == 0 or row == 6) or (col == 6 and (row != 2 and row !=3))):
+            print_C[row][col] = "*" #layout for alpha C
+    for col in range(7):
+        if (col == 0 or (row == 0 or row == 6) or ((row == 3 and col != 5 and col != 6))):
+            print_E[row][col] = "*" #layout for alpha E
+    for col in range(7):
+        if (col == 0 or (row == 0 or row == 6) and (col < 4) or (col == 4 and (row == 1 or row == 5)) or (col == 5 and (row >= 2 and row < 5))):
+            print_D[row][col] = "*" #layout for alpha D
 
-#code for E
-for row in range(5):
-    for col in range(5):
-        if (col == 0 or (row == 0 or row == 2 or row == 4) and (col > 0)):
-            print_E[row][col] = "*"
-
-#code for D
-for row in range(5):
-    for col in range(5):
-        if (col == 0 or (col == 4 and (row != 0 and row != 4)) or ((row == 0 or row == 4) and (col > 0 and col < 4))):
-            print_D[row][col] = "*"
-
-for int_col in range (5):
-    for int_row in range(5):
-        print(print_C[int_col][int_row], end = "")
+for int_col in range (7):
+    for int_row in range(7):
+        print(print_C[int_col][int_row], end = " ")
     print(end = "  ")
-    for int_row in range(5):
-        print(print_E[int_col][int_row], end = "")
+    for int_row in range(7):
+        print(print_E[int_col][int_row], end = " ")
     print(end = "  ")
-    for int_row in range(5):
-        print(print_D[int_col][int_row], end = "")
+    for int_row in range(7):
+        print(print_D[int_col][int_row], end = " ")
     print()
